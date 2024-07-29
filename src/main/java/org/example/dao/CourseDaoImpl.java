ad052e23b905389f8b8e4c64731da292c88bd6a5
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-29 11:49:11 +0300

    add CourseDao implementation

---
+++ src/main/java/org/example/dao/CourseDaoImpl.java
@@ -0,0 +1,45 @@
+package org.example.dao;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.example.entity.Course;
+import org.example.util.ConnectionManager;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CourseDaoImpl implements CourseDao {
+
+    private static CourseDaoImpl instance;
+    private static final SessionFactory sessionFactory = ConnectionManager.getConnection();
+
+    private static final String GET_ALL_NAMES_QUERY = """
+            select name from Course
+            """;
+    private static final String FIND_ALL_QUERY = """
+            from Course
+            """;
+
+    public static CourseDaoImpl getInstance() {
+        if (instance == null) {
+            instance = new CourseDaoImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public List<String> getAllNames() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(GET_ALL_NAMES_QUERY, String.class).list();
+        }
+    }
+
+    @Override
+    public List<Course> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(FIND_ALL_QUERY, Course.class).list();
+        }
+    }
+}
