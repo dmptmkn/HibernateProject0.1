7d1ef997636dd9c32ddf78a36104ac988b4fb1f0
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-05 23:21:33 +0300

    add many-to-many mapping

--- src/main/java/org/example/entity/Course.java
+++ src/main/java/org/example/entity/Course.java
@@ -2,13 +2,14 @@ package org.example.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
 import java.util.Set;
 
 @Getter
 @Setter
-@ToString
+@ToString(exclude = "students")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -32,17 +33,25 @@ public class Course {
     private Integer price;
     @Column(name = "price_per_hour")
     private Float pricePerHour;
+    @ManyToMany
+    @JoinTable(name = "subscriptions",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private Set<Student> students;
 
     @Override
-    public boolean equals(Object o) {
+    public final boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Course)) return false;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
         Course course = (Course) o;
-        return Objects.equals(id, course.id) && Objects.equals(name, course.name) && Objects.equals(duration, course.duration) && Objects.equals(description, course.description) && type == course.type && Objects.equals(teacher, course.teacher) && Objects.equals(studentsCount, course.studentsCount) && Objects.equals(price, course.price) && Objects.equals(pricePerHour, course.pricePerHour);
+        return getId() != null && Objects.equals(getId(), course.getId());
     }
 
     @Override
-    public int hashCode() {
-        return getClass().hashCode();
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
