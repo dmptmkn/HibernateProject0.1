348422b727440fe870bbc5b01d0f0d5ab8f74ded
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-07 15:44:10 +0300

    add new criteria method

--- src/main/java/org/example/dao/CourseDaoImpl.java
+++ src/main/java/org/example/dao/CourseDaoImpl.java
@@ -8,6 +8,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.graph.GraphSemantic;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaRoot;
 
 import java.util.List;
 import java.util.Map;
@@ -53,6 +56,17 @@ public class CourseDaoImpl implements CourseDao {
             Map<String, Object> properties = Map.of(GraphSemantic.LOAD.getJpaHintName(), session.getEntityGraph("withTeacher"));
             session.find(Course.class, id, properties);
             return session.find(Course.class, id, properties);
+        }
+    }
+
+    public List<Course> findAllWithDurationGT(Integer duration) {
+        try (Session session = sessionFactory.openSession()) {
+            HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+            JpaCriteriaQuery<Course> query = builder.createQuery(Course.class);
+            JpaRoot<Course> root = query.from(Course.class);
+
+            query.select(root).where(builder.gt(root.get("duration"), duration));
+            return session.createQuery(query).list();
         }
     }
 
