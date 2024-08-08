a94ba15c5a344b35d42363d9fb4f868cdb5560f7
dmptmkn <dm.ptmkn@yandex.ru>
2024-08-09 00:23:11 +0300

    add new method

--- src/main/java/org/example/printer/SubscriptionPrinter.java
+++ src/main/java/org/example/printer/SubscriptionPrinter.java
@@ -2,11 +2,13 @@ package org.example.printer;
 
 import org.example.dao.SubscriptionDao;
 import org.example.dao.SubscriptionDaoImpl;
+import org.example.entity.Course;
 import org.example.entity.Subscription;
 
 import java.io.PrintStream;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class SubscriptionPrinter extends Printer {
 
@@ -29,6 +31,23 @@ public class SubscriptionPrinter extends Printer {
             printStream.println(formattedSubscriptionInfo);
         }
         printStream.println();
+    }
+
+    public void printStudentInfo(Course course) {
+        StringJoiner infoJoiner = new StringJoiner(System.lineSeparator());
+        infoJoiner.add("Курс «" + course.getName() + "»");
+        infoJoiner.add("Информация по студентам:");
+
+        int counter = 1;
+        for (Subscription subscription : dao.getStudentInfo(course)) {
+            infoJoiner.add(String.format("%d. %s, %s, подписался %s",
+                    counter++,
+                    subscription.getStudent().getName(),
+                    getFormattedAge(subscription.getStudent().getAge()),
+                    subscription.getSubscriptionDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))));
+        }
+        infoJoiner.add(String.format("Итоговый доход с курса: ₽%d", course.getPrice() * counter));
+        printStream.println(infoJoiner);
     }
 }
 
